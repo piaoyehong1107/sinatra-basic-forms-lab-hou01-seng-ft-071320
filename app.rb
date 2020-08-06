370f40fa2921c801a4a47cbb9ace18edffa846f8
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-06 18:26:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
+  get '/create_puppy' do
+    erb :create_puppy
+  end
+  post '/puppy' do
+    erb :display_puppy
+    params[:name]
+    params[:breed]
+    params[:age]
+  end
 
 end
